e73f218c150cfd539cfce689d9e9f14f15dc823c
JiahaoWu <jiahaodev@163.com>
2020-01-07 00:35:18 +0800

    序号：01~02 简述：场景创建、玩家控制组件开发 详情：如上

---
+++ MultiplayerFPS/Assets/PlayerController.cs
@@ -0,0 +1,58 @@
+/****************************************************
+    文件：PlayerController.cs
+	作者：JiahaoWu
+    邮箱: jiahaodev@163.com
+    日期：2020/01/06 23:36
+	功能：Nothing
+*****************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        //Caculate movement velocity as a 3D vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVectical = transform.forward * _zMov;
+
+        //Final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVectical).normalized * speed;
+
+        //Apply movement
+        motor.Move(_velocity);
+
+        //Caculate rotatoin as a 3D vector( turning around)
+        float _yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.Rotate(_rotation);
+
+        //Calculate camera rotation as a 3D vector (turning around)
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        //Apply camera rotation
+        motor.RotateCamera(_cameraRotation);
+    }
+}
---
+++ MultiplayerFPS/Assets/PlayerMotor.cs
@@ -0,0 +1,71 @@
+/****************************************************
+    文件：PlayerMotor.cs
+	作者：JiahaoWu
+    邮箱: jiahaodev@163.com
+    日期：2020/01/06 23:36
+	功能：Nothing
+*****************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PreformRotation();
+    }
+
+
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PreformRotation()
+    {
+        //todo: ? 为什么角度是相乘的
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+
+}
