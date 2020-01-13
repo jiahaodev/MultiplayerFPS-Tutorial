c6220eb77244a1fca1eb8c6df2b9e4b638d4e729
JiahaoWu <jiahaodev@163.com>
2020-01-14 02:11:13 +0800

    14.添加推进器燃料机制

--- MultiplayerFPS/Assets/Scripts/PlayerController.cs
+++ MultiplayerFPS/Assets/Scripts/PlayerController.cs
@@ -23,6 +23,20 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     private float thrusterForce = 1000f;
+    [SerializeField]
+    private float thrusterFuelBurnSpeed = 1f;
+    [SerializeField]
+    private float thrusterFuelRegenSpeed = 0.3f;
+    private float thrusterFuelAmount = 1f;
+
+    public float GetThrusterFuelAmount()
+    {
+        return thrusterFuelAmount;
+    }
+
+    [SerializeField]
+    private LayerMask environmentMask;
+
 
     [Header("Spring settings:")]
     [SerializeField]
@@ -46,6 +60,20 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        //Setting target position for spring
+        //This makes the physics act right when it comes to
+        //applying gravity when flying over objects
+        RaycastHit _hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out _hit, 100f, environmentMask))
+        {
+            joint.targetPosition = new Vector3(0f, -_hit.point.y, 0f);
+        }
+        else
+        {
+            joint.targetPosition = new Vector3(0f, 0f, 0f);
+        }
+
+
         //Caculate movement velocity as a 3D vector
         float _xMov = Input.GetAxis("Horizontal");
         float _zMov = Input.GetAxis("Vertical");
@@ -57,10 +85,10 @@ public class PlayerController : MonoBehaviour
         Vector3 _velocity = (_movHorizontal + _movVectical) * speed;
 
         //Animate movement
-        animator.SetFloat("ForwardVelocity",_zMov);
+        animator.SetFloat("ForwardVelocity", _zMov);
 
         //Apply movement
-        motor.Move(_velocity); 
+        motor.Move(_velocity);
 
         //Caculate rotatoin as a 3D vector( turning around)
         float _yRot = Input.GetAxisRaw("Mouse X");
@@ -79,15 +107,23 @@ public class PlayerController : MonoBehaviour
 
         //Calculate the thrusterforce based on player input
         Vector3 _thrusterForce = Vector3.zero;
-        if (Input.GetButton("Jump"))
+        if (Input.GetButton("Jump") && thrusterFuelAmount > 0f)
         {
-            _thrusterForce = Vector3.up * thrusterForce;
-            SetJointSettings(0f);
+            thrusterFuelAmount -= thrusterFuelBurnSpeed * Time.deltaTime;
+
+            if (thrusterFuelAmount >= 0.01f)
+            {
+                _thrusterForce = Vector3.up * thrusterForce;
+                SetJointSettings(0f);
+            }
         }
         else
         {
+            thrusterFuelAmount += thrusterFuelRegenSpeed * Time.deltaTime;
             SetJointSettings(jointSpring);
         }
+
+        thrusterFuelAmount = Mathf.Clamp(thrusterFuelAmount, 0f, 1f);
 
         //Apply the thruster force
         motor.ApplyThruster(_thrusterForce);
@@ -96,7 +132,8 @@ public class PlayerController : MonoBehaviour
 
     private void SetJointSettings(float _jointSpring)
     {
-        joint.yDrive = new JointDrive {
+        joint.yDrive = new JointDrive
+        {
             positionSpring = _jointSpring,
             maximumForce = jointMaxForce
         };
---
+++ MultiplayerFPS/Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,35 @@
+/****************************************************
+    文件：PlayerUI.cs
+	作者：JiahaoWu
+    邮箱: jiahaodev@163.com
+    日期：2020/01/14 01:20
+	功能：Player UI Canvas 管理
+*****************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour 
+{
+    [SerializeField]
+    RectTransform thrusterFuelFill;
+
+    private PlayerController controller;
+
+    public void SetController(PlayerController _controller) {
+        controller = _controller;
+    }
+
+    private void SetFuelAmount(float _amount)
+    {
+        thrusterFuelFill.localScale = new Vector3(1f,_amount,1f);
+    }
+
+    private void Update()
+    {
+        SetFuelAmount(controller.GetThrusterFuelAmount());
+    }
+
+
+}
